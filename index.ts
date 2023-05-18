8ccf18f2a9781a447196af28978ea5b4a8ab8d6f
balkacer <balcacer.enmanuel@gmail.com>
2023-05-18 14:42:28 -0400

    [main] - Adding Socket.io

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, json } from 'express';
+import http from 'http';
+import { Server } from "socket.io";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(json())
+
+app.post('/say_hi', (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  res.json({ msg: `Hi, ${name}!` }).sendStatus(200)
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat_message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
